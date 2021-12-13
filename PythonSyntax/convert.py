cc758342fd8d627ba3776afec5f46ae6aa828d9d
upluke <lukeforadream@gmail.com>
2021-12-12 21:44:13 -0800

    any 7 convert

--- PythonSyntax/convert.py
+++ PythonSyntax/convert.py
@@ -16,11 +16,18 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    if unit_in != "c" and unit_in != "f":
+        return f"Invalid unit {unit_in}"
+    elif unit_out != "c" and unit_out != "f":
+        return f"Invalid unit {unit_out}"
+    else:
+        return "{:.1f}".format((temp*1.8)+32) if unit_in == 'c' else "{:.1f}".format((temp-32)*0.5556)
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
+
+
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
 print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
-
