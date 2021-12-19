054215b052fe6664d4c36fc3b24475a457603800
upluke <lukeforadream@gmail.com>
2021-12-19 15:47:57 -0800

    03 04 05 06

--- python-ds-practice 2/03_last_element/last_element.py
+++ python-ds-practice 2/03_last_element/last_element.py
@@ -1,9 +1,14 @@
 def last_element(lst):
     """Return last item in list (None if list is empty.
-    
+
         >>> last_element([1, 2, 3])
         3
-        
+
         >>> last_element([]) is None
         True
     """
+    return lst.pop() if len(lst) > 0 else None
+
+
+print(last_element([1, 2, 3]))
+print(last_element([]))
--- python-ds-practice 2/05_reverse_string/reverse_string.py
+++ python-ds-practice 2/05_reverse_string/reverse_string.py
@@ -1,3 +1,4 @@
+# https://www.journaldev.com/23647/python-reverse-string
 def reverse_string(phrase):
     """Reverse string,
 
@@ -7,3 +8,12 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+    # return phrase[::-1]
+    # return ''.join(reversed(phrase))
+    if len(phrase) == 1:
+        return phrase
+    return reverse_string(phrase[1:]) + phrase[0]
+
+
+print(reverse_string('awesome'))
+print(reverse_string('sauce'))
