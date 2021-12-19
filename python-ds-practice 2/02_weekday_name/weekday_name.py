25df2533cd4a14f2b40e49b84c9484819d6a988b
upluke <lukeforadream@gmail.com>
2021-12-18 22:17:17 -0800

    weekday name

--- python-ds-practice 2/02_weekday_name/weekday_name.py
+++ python-ds-practice 2/02_weekday_name/weekday_name.py
@@ -1,14 +1,23 @@
 def weekday_name(day_of_week):
     """Return name of weekday.
-    
+
         >>> weekday_name(1)
         'Sunday'
-        
+
         >>> weekday_name(7)
         'Saturday'
-        
+
     For days not between 1 and 7, return None
-    
+
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    hp = {1: 'Sunday', 2: 'Tuesday', 3: 'Wednesday',
+          4: 'Thursday', 5: 'Friday', 6: 'Saturaday', 7: 'Saturday'}
+    return hp[day_of_week] if day_of_week >= 1 and day_of_week <= 7 else None
+
+
+print(weekday_name(1))
+print(weekday_name(7))
+print(weekday_name(9))
+print(weekday_name(0))
