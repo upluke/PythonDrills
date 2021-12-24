a2d9185df3f291d10febf393cc72b9255a2ebad3
upluke <lukeforadream@gmail.com>
2021-12-24 15:09:19 -0800

    flip_case

--- python-ds-practice 2/11_flip_case/flip_case.py
+++ python-ds-practice 2/11_flip_case/flip_case.py
@@ -11,3 +11,19 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    # if ord(to_swap)<97:
+    #     phrase.replace(to_swap,'_')
+    # else:
+    phrase = phrase.replace(to_swap, '_')
+    if ord(to_swap) >= 97:
+        phrase = phrase.replace(to_swap.upper(), to_swap)
+        phrase = phrase.replace('_', to_swap.upper())
+    else:
+        phrase = phrase.replace(to_swap.lower(), to_swap)
+        phrase = phrase.replace('_', to_swap.lower())
+    return phrase
+
+
+print(flip_case('Aaaahhh', 'a'))
+print(flip_case('Aaaahhh', 'A'))
+print(flip_case('Aaaahhh', 'h'))
