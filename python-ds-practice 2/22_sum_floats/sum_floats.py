5db147872505f9b8faeaad21216ec96beeb570d6
upluke <lukeforadream@gmail.com>
2022-01-02 20:40:18 -0800

    sum floats

--- python-ds-practice 2/22_sum_floats/sum_floats.py
+++ python-ds-practice 2/22_sum_floats/sum_floats.py
@@ -1,9 +1,12 @@
+import functools
+
+
 def sum_floats(nums):
     """Return sum of floating point numbers in nums.
-    
+
         >>> sum_floats([1.5, 2.4, 'awesome', [], 1])
         3.9
-        
+
         >>> sum_floats([1, 2, 3])
         0
     """
@@ -11,3 +14,9 @@ def sum_floats(nums):
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+    return functools.reduce(lambda accu, curr: accu +
+                            curr if isinstance(curr, float) else accu, nums, 0)
+
+
+print(sum_floats([1.5, 2.4, 'awesome', [], 1]))
+print(sum_floats([1, 2, 3]))
