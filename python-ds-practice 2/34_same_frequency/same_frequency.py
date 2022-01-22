da39320d45cef49b5c70481c1ba9129a10f657d3
upluke <lukeforadream@gmail.com>
2022-01-22 12:57:29 -0800

    same frequencey

--- python-ds-practice 2/34_same_frequency/same_frequency.py
+++ python-ds-practice 2/34_same_frequency/same_frequency.py
@@ -1,12 +1,24 @@
 def same_frequency(num1, num2):
     """Do these nums have same frequencies of digits?
-    
+
         >>> same_frequency(551122, 221515)
         True
-        
+
         >>> same_frequency(321142, 3212215)
         False
-        
+
         >>> same_frequency(1212, 2211)
         True
     """
+    hp = {}
+    for n in str(num1)+str(num2):
+        hp[n] = hp.get(n, 0)+1
+    for value in hp.values():
+        if value % 2 == 1:
+            return False
+    return True
+
+
+print(same_frequency(551122, 221515))
+print(same_frequency(321142, 3212215))
+print(same_frequency(1212, 2211))
