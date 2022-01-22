b7ec855528f8a0e5d3fc39a5141f51055f22e33e
upluke <lukeforadream@gmail.com>
2022-01-22 00:07:04 -0800

    two list dictionary

--- python-ds-practice 2/27_titleize/titleize.py
+++ python-ds-practice 2/27_titleize/titleize.py
@@ -7,3 +7,11 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    res = ""
+    for word in phrase.split(' '):
+        res += " " + word.capitalize()
+    return res
+
+
+print(titleize('this is awesome'))
+print(titleize('oNLy cAPITALIZe fIRSt'))
