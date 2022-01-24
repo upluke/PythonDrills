a2e5c06153096d1f3306db62c5950d73e1f13811
upluke <lukeforadream@gmail.com>
2022-01-23 17:17:58 -0800

    sum up diagnolas

--- python-ds-practice 2/37_sum_up_diagonals/sum_up_diagonals.py
+++ python-ds-practice 2/37_sum_up_diagonals/sum_up_diagonals.py
@@ -18,3 +18,26 @@ def sum_up_diagonals(matrix):
         >>> sum_up_diagonals(m2)
         30
     """
+    l, r, res = 0, len(matrix)-1, 0
+
+    for _ in matrix:
+        res += matrix[l][l]
+        res += matrix[l][r]
+        l += 1
+        r -= 1
+    return res
+
+
+m1 = [
+    [1,   2],
+    [30, 40],
+]
+print(sum_up_diagonals(m1))
+
+
+m2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+print(sum_up_diagonals(m2))
