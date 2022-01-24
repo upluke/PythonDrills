ceffd4692e4486743baf044bdaf90ad727157997
upluke <lukeforadream@gmail.com>
2022-01-23 16:39:14 -0800

    find the duplicate

--- python-ds-practice 2/36_find_the_duplicate/find_the_duplicate.py
+++ python-ds-practice 2/36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,22 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    hp = {}
+
+    for num in nums:
+        hp[num] = hp.get(num, 0)+1
+
+    for key, value in hp.items():
+        if value > 1:
+            return key
+
+    return None
+
+
+print(find_the_duplicate([1, 2, 1, 4, 3, 12]))
+
+
+print(find_the_duplicate([6, 1, 9, 5, 3, 4, 9]))
+
+
+print(find_the_duplicate([2, 1, 3, 4]))
