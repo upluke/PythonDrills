e0b8d92e7ae17e6f4d5287b9d7c77132d6179570
upluke <lukeforadream@gmail.com>
2022-01-26 10:08:44 -0800

    reverse vowels

--- python-ds-practice 2/fs_4_reverse_vowels/reverse_vowels.py
+++ python-ds-practice 2/fs_4_reverse_vowels/reverse_vowels.py
@@ -19,3 +19,17 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    l, r = 0, len(s)-1
+    s_list = list(s)
+    vowels = "aeiouAEIOU"
+    while l < r:
+        if s_list[l] in vowels and s_list[r] in vowels:
+            s_list[l], s_list[r] = s_list[r], s_list[l]
+            l += 1
+            r -= 1
+        elif s_list[l] not in vowels:
+            l += 1
+        elif s_list[r] not in vowels:
+            r -= 1
+
+    return "".join(s_list)
