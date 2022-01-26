cca7fffe4a9e10c9b1b496d6f2c6173c92eaf747
upluke <lukeforadream@gmail.com>
2022-01-25 23:47:42 -0800

    greater numbers

--- python-ds-practice 2/39_find_greater_numbers/find_greater_numbers.py
+++ python-ds-practice 2/39_find_greater_numbers/find_greater_numbers.py
@@ -19,3 +19,16 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    count = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] > nums[i]:
+                count += 1
+
+    return count
+
+
+find_greater_numbers([6, 1, 2, 7])
+find_greater_numbers([5, 4, 3, 2, 1])
+find_greater_numbers([1, 2, 3])
