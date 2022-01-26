8c9466b061a092b4fd1851f37d617c67e578df70
upluke <lukeforadream@gmail.com>
2022-01-26 10:06:52 -0800

    three odd numbers

--- python-ds-practice 2/fs_3_three_odd_numbers/three_odd_numbers.py
+++ python-ds-practice 2/fs_3_three_odd_numbers/three_odd_numbers.py
@@ -13,3 +13,9 @@ def three_odd_numbers(nums):
         >>> three_odd_numbers([1, 2, 3, 3, 2])
         False
     """
+    i = 0
+    while (i+2) < len(nums):
+        if sum(nums[i:i+3]) % 2 == 1:
+            return True
+        i += 1
+    return False
