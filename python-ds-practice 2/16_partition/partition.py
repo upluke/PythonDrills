1f49592efeebc1848e5fc8a05e205b4efc161ac1
upluke <lukeforadream@gmail.com>
2022-02-02 22:13:29 -0800

    partition

--- python-ds-practice 2/16_partition/partition.py
+++ python-ds-practice 2/16_partition/partition.py
@@ -19,6 +19,28 @@ def partition(lst, fn):
         >>> partition(["hi", None, 6, "bye"], is_string)
         [['hi', 'bye'], [None, 6]]
     """
+
+    # solution:
+
+    # true_list = []
+    # false_list = []
+
+    # for val in lst:
+    #     if fn(val):
+    #         true_list.append(val)
+    #     else:
+    #         false_list.append(val)
+
+    # return [true_list, false_list]
+
+    # Clever, but less optimal solution --- this runs fn() twice on each element,
+    # not once:
+    #
+    # return [
+    #     [val for val in lst if fn(val)],
+    #     [val for val in lst if not fn(val)]
+    # ]
+
     temp = []
     idx = 0
     while idx < len(lst):
