97fb5bcbe810422578de7a52a778cb20532ccb68
upluke <lukeforadream@gmail.com>
2022-02-02 22:24:12 -0800

    mode

--- python-ds-practice 2/17_mode/mode.py
+++ python-ds-practice 2/17_mode/mode.py
@@ -11,6 +11,23 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    # solution:
+    # # Make frequency counter of num:freq
+    # counts = {}
+
+    # for num in nums:
+    #     counts[num] = counts.get(num, 0) + 1
+
+    # # find the highest value (the most frequent number)
+
+    # max_value = max(counts.values())
+
+    # # now we need to see at which index the highest value is at
+
+    # for (num, freq) in counts.items():
+    #     if freq == max_value:
+    #         return num
+
     most_count = 0
     for num in nums:
         if most_count < nums.count(num):
