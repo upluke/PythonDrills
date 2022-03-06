8f35b853535772e52956807ac7206e2e334fc036
upluke <lukeforadream@gmail.com>
2022-03-05 22:16:53 -0800

    valid parentheses

--- python-ds-practice 2/fs_2_valid_parentheses/valid_parentheses.py
+++ python-ds-practice 2/fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,28 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    # solution:
+    # count = 0
+
+    # for p in parens:
+    #     if p == '(':
+    #         count += 1
+    #     elif p == ')':
+    #         count -= 1
+
+    #     # fast fail: too many right parens
+    #     if count < 0:
+    #         return False
+
+    # return count == 0
+
+    open_paranthesis = []
+    for parant in parens:
+        if parant == '(':
+            open_paranthesis.append(parant)
+        else:
+            if len(open_paranthesis):
+                open_paranthesis.pop()
+            else:
+                return False
+    return True if len(open_paranthesis) == 0 else False
