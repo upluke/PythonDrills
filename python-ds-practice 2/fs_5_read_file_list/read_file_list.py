ac368c110e4739328d87b69a006ea0462f3372db
upluke <lukeforadream@gmail.com>
2022-03-09 22:21:32 -0800

    read file solu

--- python-ds-practice 2/fs_5_read_file_list/read_file_list.py
+++ python-ds-practice 2/fs_5_read_file_list/read_file_list.py
@@ -19,6 +19,14 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+
+    # solution:
+    # with open(filename) as f:
+    # for line in f:
+    #     # remove newline at end of line!
+    #     line = line.strip()
+    #     print(f"- {line}")
+
     my_words = open(filename)
     for word in my_words:
         if len(word) > 1:
